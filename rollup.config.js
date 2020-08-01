451cc41efbdbcabf4fce1ad2099e84d3bff4967b
lilong <lilong@gmail.com>
2020-08-01 11:54:21 +0900

    common plugin

--- rollup.config.js
+++ rollup.config.js
@@ -17,34 +17,34 @@ export default [
       {
         file: pkg.main,
         format: 'cjs',
-        sourcemap: true,
-        plugins: [
-          getBabelOutputPlugin({
-            presets: [
-              ['@babel/preset-env',
-                {
-                  debug: true,
-                  useBuiltIns: 'usage',
-                  shippedProposals: true,
-                  targets: {
-                    node: '8.10',
-                    edge: '17',
-                    firefox: '60',
-                    chrome: '67',
-                    safari: '11.1'
-                  },
-                  corejs: 3
-                }]
-            ],
-            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
-          })
-        ]
+        sourcemap: true
       },
       {
         file: pkg.module,
         format: 'es',
         sourcemap: true
       }
+    ],
+    plugins: [
+      getBabelOutputPlugin({
+        presets: [
+          ['@babel/preset-env',
+            {
+              debug: true,
+              useBuiltIns: 'usage',
+              shippedProposals: true,
+              targets: {
+                node: '8.10',
+                edge: '17',
+                firefox: '60',
+                chrome: '67',
+                safari: '11.1'
+              },
+              corejs: 3
+            }]
+        ],
+        plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
+      })
     ]
   }
 ]
